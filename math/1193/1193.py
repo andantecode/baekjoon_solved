0078d386fb58cbbc2dedc2ad08473091b889e951
andantecode <kb721a@naver.com>
2024-08-22 14:35:41 +0900

    solved 1193

--- math/1193/1193.py
+++ math/1193/1193.py
@@ -4,6 +4,7 @@
 # 1행 기준으로만 대각선으로 읽어간다고 생각하면 1행의 값들은 1, 2, 4, 7, 11, 16, ...
 # 각 잔차가 1, 2, 3, 4, 5, ...
 # target을 입력받아 해당 값을 세기 직전의 1행의 값 + k로 나타낼 수 있음
+# ex) 12 = 11(index = 5) + 1 -- 편의를 위해 index=1부터 생각
 # 해당 열이 짝수일 경우
 # 1행의 열을 col로 나타내면 (1, col) 기준으로 (1+k, col-k)로 나타낼 수 있음
 # 해당 열이 홀수일 경우
@@ -26,8 +27,8 @@ for i in range(maximum_x):
 k = target - list_[-2]
 index = len(list_[:-1])
 
-print(list_)
-print(f'k = {k}, index = {index}')
+# print(list_)
+# print(f'k = {k}, index = {index}')
 
 if index % 2 == 0:
     print(f'{1 + k}/{index - k}')
