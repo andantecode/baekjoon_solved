ed78cba7d50ed9ee088a7f3250ad386cd4402d6d
andantecode <kb721a@naver.com>
2024-08-27 00:19:40 +0900

    solved 10815

--- setandmap/10815.py
+++ setandmap/10815.py
@@ -6,8 +6,6 @@ my_card = list(map(int, sys.stdin.readline().rstrip('\n').split(' ')))
 
 my_card = {item: 1 for item in my_card}
 
-print(my_card)
-
 m = int(input())
 
 validation_card = list(map(int, sys.stdin.readline().rstrip('\n').split(' ')))
